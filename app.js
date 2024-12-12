b730148f919576bbb233859d9ee64d24f6b1b18f
wassupdee <daichi.hirohata.japan@gmail.com>
2024-12-12 13:58:43 +0900

    イベント情報と独自の引数をメソッドに渡す

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ const app = Vue.createApp({
     toggleShowBooks() {
       this.showBooks = !this.showBooks
     },
-    handleEvent(e) {
+    handleEvent(e, data) {
       console.log(e)
+      if (data) {
+        console.log(data)
+      }
     }
   }
 })
