aea5673695d4851a217c671ca345638c11e02252
wassupdee <daichi.hirohata.japan@gmail.com>
2024-12-12 11:53:41 +0900

    関数を使って、falsyにする

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
   data(){
     return {
+      showBooks: true,
       title: 'The Final Empire',
       author: 'Brandon  Sanderson',
       age: 45
     }
   },
-  methods:{
-    changeTitle(title) {
-      // this.title = 'Words of Randiance'
-      this.title = title
+  methods: {
+    toggleShowBooks(){
+      this.showBooks = false
     }
   }
 })
